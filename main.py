64d95f7b73ae6eaed1112ba9aab2a823c6abbe2e
Dudu <dididudu@users.noreply.github.com>
2014-05-08 11:38:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# AMOUL SOLO PROJECT
+#
+
+__author__ = 'Didier Dulac'
+
+import webapp2
+
+from actions import BaseRequestHandler
+
+class MainPage(BaseRequestHandler):
+  def get(self):
+    template_values = {
+      }
+    self.generate('index.html', template_values)
+
+application = webapp2.WSGIApplication([
+  ('/', MainPage)
+], debug=True)
